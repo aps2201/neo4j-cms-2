df0602b6578323c38f23793333ed9649bd7227d3
aps2201 <6870663+aps2201@users.noreply.github.com>
2026-05-24 12:19:07 +0800

    added write date

--- cmd/new_post.go
+++ cmd/new_post.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"time"
 
 	"charm.land/huh/v2"
 	"github.com/google/uuid"
@@ -19,8 +20,10 @@ var write_post = &cobra.Command{
 	Use:   "write-post",
 	Short: "Write a new post",
 	Run: func(cmd *cobra.Command, args []string) {
+		var post_created string
 		var post_title string
 		var post_content string
+		post_created = time.Now().Format("20060102150405")
 		form := huh.NewForm(
 			huh.NewGroup(
 				huh.NewInput().Title("Title").Value(&post_title).Validate(func(s string) error {
@@ -40,13 +43,13 @@ var write_post = &cobra.Command{
 		if err := form.Run(); err != nil {
 			slog.Error("Form failed", ":", err)
 		}
-		post_id := writeNewPost(post_title, post_content)
+		post_id := writeNewPost(post_title, post_content, post_created)
 		cmd.Println(post_id)
 
 	},
 }
 
-func writeNewPost(post_title string, post_content string) (post_id string) {
+func writeNewPost(post_title string, post_content string, post_created string) (post_id string) {
 	var post_uuid uuid.UUID
 	post_uuid, _ = uuid.NewRandom()
 	post_id = post_uuid.String()
@@ -55,10 +58,12 @@ func writeNewPost(post_title string, post_content string) (post_id string) {
 	_, err := neo4j.ExecuteQuery(ctx, d, `
 	CREATE (p:Post {post_id:$post_id, source:"cms"}) 
 	SET p.title = $post_title,
-		p.content 	= $post_content
+		p.content 	= $post_content,
+		p.created = $post_created
 	`, map[string]any{"post_id": post_id,
 		"post_title":   post_title,
 		"post_content": post_content,
+		"post_created": post_created,
 	}, neo4j.EagerResultTransformer)
 	if err != nil {
 		slog.Error("cant execute query", "error:", err)
